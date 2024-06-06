00da9b4bd02366006970328d82ea6a55b4de01a7
SHIVA <pabbalas7@gmail.com>
2024-06-06 11:34:57 +0530

    runing

---
+++ 06Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+NUM=$1
+
+if [ $NUM -gt 20 ]
+then 
+ echo "$NUM is graterthan 20 "
+else
+ echo "$NUM is lessthan 20 "
+fi  
---
+++ 07-install-mysql.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ID=$(id -u)
+
+if[ $ID -ne 0 ]
+then
+ echo "ERROR: please run script with root access"
+ exit 1
+else
+ echo "your  root user"
+fi 
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+ echo "installing mysql is failed"
+else
+ echo "installing mysql i success"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+ echo "installing git is failed"
+else
+ echo "installing git i success"
+fi
