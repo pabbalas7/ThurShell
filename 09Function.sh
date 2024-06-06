a2d3f0fffd723cb2e40dde88d3452540e288aed4
SHIVA <pabbalas7@gmail.com>
2024-06-06 13:28:53 +0530

    hh

--- 09Function.sh
+++ 09Function.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+ID=$(id -u)
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-     echo "ERROR:: $2... is failed"
-     exit 1
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
     else
-     echo "$1... is success"
+        echo "$2 ... SUCCESS"
     fi
 }
+
 if [ $ID -ne 0 ]
 then
- echo "ERROR: Please run scripit witn root acces"
- exit 1
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
 else
- echo "your rooot user"
-fi
-
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y
 
-VALIDATE $? "Instlling SQL"
+VALIDATE $? "Installing MySQL"
 
 yum install git -y
 
