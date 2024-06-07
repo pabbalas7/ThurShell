2398f8e17f40c3663e5fb0df69a7a3f775164a83
SHIVA <pabbalas7@gmail.com>
2024-06-07 07:31:22 +0530

    jj

--- 12-loops2.sh
+++ 12-loops2.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started  exceted $TIMESTAMP &>>LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo -e "ERROR:: $2 ... $R Eailed $N "
-        exit 1
+    then
+        echo -e "$2 ... $R FAILED $N"
     else
-        echo -e "$2 .. $G  success $N "
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
 if [ $ID -ne 0 ]
-then 
-  echo -e $R ERROR:: Please run this script with root access $N 
-  exit 1
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-  echo -e "You are root user"
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
+
 for package in $@
 do
     yum list installed $package &>> $LOGFILE #check installed or not
